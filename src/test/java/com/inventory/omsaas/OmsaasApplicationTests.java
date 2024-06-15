d43c288184e8b6e6e2c0d0b9769a48eed4dce4d7
Manu Kashysp <manukashysp@Manus-MacBook-Air.local>
2024-06-15 16:57:35 +0530

    Initial commit for omsaas project

---
+++ src/test/java/com/inventory/omsaas/OmsaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory.omsaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
